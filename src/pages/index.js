575b6cd7c5a48c51d2475e5647fdf702c3b6b79c
Nicolas Rodriguez <nicolasrodriguezch@hotmail.com>
2023-05-11 14:37:31 -0500

    starts the next migration

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import {Head} from 'next/head';
+
+export default function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>Agora</title>
+      </Head>
+      <div>Hello, this is HomePage!</div>
+    </>
+  );
+}
